0a8ba47303a1ca42251b0a3f8b7f3f6ddea38222
Mohammed Azmy <mohamed.azmy@thebetechnology.com>
2010-05-11 11:03:58 +0300

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(name='pixel',
+      version='0.1',
+      description='Another python xml library',
+      author='Mohamed Azmy',
+      author_email='mohammed.azmy@yahoo.com',
+      url='http://github.com/mohammedazmy/pixel',
+      packages=['pixel'],
+      package_dir = {'': 'src'})
